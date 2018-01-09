b90d83ad0732f861b4b3361cd27190bcdc639a73
Irfan Fakharidzaky Rizal <35243907+irfanfrizal@users.noreply.github.com>
2018-01-09 11:47:23 +0700

    Add files via upload

---
+++ C++ AWAL PERTEMPERKENALAN.cpp
@@ -0,0 +1,13 @@
+#include <conio.h>
+#include <iostream.h>
+
+main()
+{
+  cout <<"***PERKENALAN***\n";
+  cout <<"================\n";
+  cout <<"NAMA SAYA : Irfan Fakharidzaky Rizal\n";
+  cout <<"NIM saya  : 12171329\n";
+  cout <<"ALAMAT SAYA: BSD CITY\n";
+  getch();
+
+}
