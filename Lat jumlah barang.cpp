b4eefc0a92479ecea6bd239e2347eabb58609ca8
Irfan Fakharidzaky Rizal <35243907+irfanfrizal@users.noreply.github.com>
2018-01-09 11:50:56 +0700

    Add files via upload

---
+++ Lat jumlah barang.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+#include <iostream.h>
+
+main()
+{
+ int jumbar1=150,jumbar2=23;
+
+ clrscr( );
+ cout<<"Jumlah barang 1; "<<jumbar1;
+ cout<<"jumlah barang 2; "<<jumbar2;
+
+ getch( );
+}
